29df02351bbd1be3a3001e9eabd9b9fcdc02a9aa
Avishay Bar <avishay.bar@cyberark.com>
2019-01-07 20:06:15 +0000

    - Updated wrong path on pester

--- tests/integration/default/pester/defaults.tests.ps1
+++ tests/integration/default/pester/defaults.tests.ps1
@@ -1,6 +1,6 @@
 describe "ansible_test_kitchen_windows_role ansible role" {
     Context "CPM Installation Path" {
-        $Path = "C:\Program Files (x86)\Cyberark\Central Policy Manager"
+        $Path = "C:\Program Files (x86)\Cyberark\Password Manager"
         it "CPM Directory Exists" {
             Test-Path -Path $Path | Should be $true
         }
